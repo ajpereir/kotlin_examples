cc43796f9cc8e8ab604727cba0efc3c9168014a8
ajpereir <ajpereira@criticalsoftware.com>
2019-07-23 13:35:06 +0100

    Small changes

--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,20 @@
 import examples.CalculationsExample
+import examples.ExampleType
 import examples.ExampleTypeFactory
 import examples.ExamplesManager
 
 fun main(args: Array<String>) {
-//    ExamplesManager.invoke(ExampleType.CALCULATIONS_STRATEGY)
-//    ExampleTypeFactory.CALCULATIONS_STRATEGY.invoke()
+    val divider = "-------------------"
+    println(divider)
+    ExamplesManager.executeExample(ExampleType.CALCULATIONS_STRATEGY)
+    println(divider)
+    println(divider)
+    ExampleTypeFactory.CALCULATIONS_STRATEGY()
+    println(divider)
+    println(divider)
     ExamplesManager.executeExample { CalculationsExample() }
-//    ExamplesManager.executeExample(CalculationsExample)
+    println(divider)
+    println(divider)
+    ExamplesManager.executeExample(CalculationsExample)
+    println(divider)
 }
